f34f58bd74bc76ea8cee302b73d6436ec94ea45f
Vitaly Nelyub <nelyub86@ukr.net>
2023-04-04 23:37:59 +0300

    list

--- src/components/Contacts/ContactList.jsx
+++ src/components/Contacts/ContactList.jsx
@@ -1,23 +1,18 @@
-import { Component } from 'react';
 import css from '../Contacts/ContactList.module.css';
 
-class ContactList extends Component {
-  render() {
-      return this.props.filteredContact.map(contact => (
-        <li key={contact.id} className={css.contact__item}>
-          <p>{contact.name}:</p>
-          <span>{contact.number}</span>
-          <button
-            type="button"
-            onClick={() => this.props.deleteContact(contact.id)}
-            className={css.delete__btn}
-            id={contact.id}
-          >
-            Delete
-          </button>
-        </li>
-      ));
-  }
+export default function ContactList({ filteredContact, deleteContact }) {
+  return filteredContact.map(contact => (
+    <li key={contact.id} className={css.contact__item}>
+      <p>{contact.name}:</p>
+      <span>{contact.number}</span>
+      <button
+        type="button"
+        onClick={() => deleteContact(contact.id)}
+        className={css.delete__btn}
+        id={contact.id}
+      >
+        Delete
+      </button>
+    </li>
+  ));
 }
-
-export default ContactList;
